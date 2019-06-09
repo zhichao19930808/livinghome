77be7180b6e138ed6bfa06d16a6e9fa149e346e3
zhichao19930808 <zhichaoglory@163.com>
2019-06-09 15:05:28 +0800

    Initial commit

---
+++ src/main/java/xin/zcglory/livinghome/LivinghomeApplication.java
@@ -0,0 +1,13 @@
+package xin.zcglory.livinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivinghomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivinghomeApplication.class, args);
+	}
+
+}
